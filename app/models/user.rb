aeac2c9853b4eadbfc932bc1b0312dc33af256d8
Mendel Stillerman <menstiler@gmail.com>
2019-08-24 22:44:25 -0400

    validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-  has_many :user_spaces
+  has_many :user_spaces, dependent: :destroy
   has_many :spaces, through: :user_spaces
   has_many :messages
   has_many :chatrooms, through: :messages
-  has_many :notifications
+  has_many :notifications, dependent: :destroy
   has_many :favorites
 
+  validates :username, presence: true
   validates :username, uniqueness: true
+  validates :name, presence: true
   has_secure_password
 end
